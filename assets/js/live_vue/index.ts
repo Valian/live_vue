d62cd000bbe82fdf562986413b3a7defbcccd102
Valian <jakub.skalecki@gmail.com>
2025-03-18 22:57:16 +0100

    WIP useLiveForm

--- assets/js/live_vue/index.ts
+++ assets/js/live_vue/index.ts
@@ -12,5 +12,6 @@ export type {
 export { createLiveVue } from "./app.js"
 export { getHooks } from "./hooks.js"
 export { useLiveVue, useLiveEvent, useLiveNavigation, useLiveUpload } from "./use.js"
+export { useLiveForm, useFieldArray, type Form } from "./useLiveForm.js"
 export { findComponent } from "./utils.js"
 export { default as Link } from "./link.js"
