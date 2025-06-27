daac47b13cee2b832db5521572c0a002c4a9500a
Jakub Skałecki <jakub.skalecki@gmail.com>
2025-06-27 14:49:02 +0200

    feature: useLiveEvent composable (#58)

--- assets/js/live_vue/use.ts
+++ assets/js/live_vue/use.ts
@@ -1,4 +1,4 @@
-import { inject } from "vue"
+import { inject, onMounted, onUnmounted } from "vue"
 import type { LiveHook } from "./types.js"
 
 export const liveInjectKey = "_live_vue"
@@ -12,4 +12,23 @@ export const useLiveVue = (): LiveHook => {
   const live = inject<LiveHook>(liveInjectKey)
   if (!live) throw new Error("LiveVue not provided. Are you using this inside a LiveVue component?")
   return live
+}
+
+/**
+ * Registers a callback to be called when an event is received from the server.
+ * It automatically removes the callback when the component is unmounted.
+ * @param event - The event name.
+ * @param callback - The callback to call when the event is received.
+ */
+export function useLiveEvent<T>(event: string, callback: (data: T) => void) {
+  let callbackRef: ReturnType<LiveHook["handleEvent"]> | null = null
+  onMounted(() => {
+    const live = useLiveVue()
+    callbackRef = live.handleEvent(event, callback)
+  })
+  onUnmounted(() => {
+    const live = useLiveVue()
+    if (callbackRef) live.removeHandleEvent(callbackRef)
+    callbackRef = null
+  })
 }
