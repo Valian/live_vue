9f9c1d0503c9f9cbc889ac63befe3c3f5e4b13ff
Valian <jakub.skalecki@gmail.com>
2025-06-30 17:47:30 +0200

    update phoenix_live_view in example project

--- example_project/mix.exs
+++ example_project/mix.exs
@@ -35,7 +35,7 @@ defmodule LiveVueExamples.MixProject do
       {:phoenix, "~> 1.7.12"},
       {:phoenix_html, "~> 4.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.20.2"},
+      {:phoenix_live_view, "~> 1.0"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:heroicons,
