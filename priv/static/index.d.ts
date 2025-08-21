08ddaa9137259a1af5e84543c2dd97acf518bc4c
Valian <jakub.skalecki@gmail.com>
2025-08-21 11:25:23 +0200

    removed temporary compiled files

--- priv/static/index.d.ts
+++
@@ -1,7 +0,0 @@
-export type { LiveVueApp, LiveVueOptions, SetupContext, VueComponent, LiveHook, ComponentMap, UploadConfig, UploadEntry, UploadOptions, } from "./types.js";
-export { createLiveVue } from "./app.js";
-export { getHooks } from "./hooks.js";
-export { useLiveVue, useLiveEvent, useLiveNavigation, useLiveUpload } from "./use.js";
-export { useLiveForm, type Form, type FormField, type FormFieldArray, type FormOptions, type UseLiveFormReturn } from "./useLiveForm.js";
-export { findComponent } from "./utils.js";
-export { default as Link } from "./link.js";
